868c9534ba37e0d91653ffb5717f06fd3bac56be
iamsamienda <samuelendale7373@gmail.com>
2023-11-09 00:05:23 +0300

    created new pipfile

---
+++ tags/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+# Create your models here.
+
+
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+    
+    
+    
+class TaggedItem(models.Model):
+    # what tag applied to what object
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    # type (product, video, article)---using we find the table
+    # id ---using we can find the record
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
