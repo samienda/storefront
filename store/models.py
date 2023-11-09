5f789e9feb8f89cdaef285934f29ac8252387c14
iamsamienda <samuelendale7373@gmail.com>
2023-11-09 20:19:54 +0300

    changed the model order

--- store/models.py
+++ store/models.py
@@ -66,6 +66,13 @@ class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
     
 
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unitprice = models.DecimalField(max_digits=6, decimal_places=2)
+
+
 
 class  Address(models.Model):
     street = models.CharField(max_length=255)
@@ -78,15 +85,7 @@ class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     
     
-    
-    
-class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.PROTECT)
-    product = models.ForeignKey(Product, on_delete=models.PROTECT)
-    quantity = models.PositiveSmallIntegerField()
-    unitprice = models.DecimalField(max_digits=6, decimal_places=2)
-    
-    
+           
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
